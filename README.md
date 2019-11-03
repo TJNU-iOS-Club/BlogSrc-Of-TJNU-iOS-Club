b324cb8c4076cb38f2cddac32a96ba74900910b1
kim yang <942326994@qq.com>
2019-11-03 18:05:05 +0800

    :wrench: 增加流水线

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# 天津师范大学 C422 创新创业工作室主页
+# 天津师范大学 iOS Club 主页
 
-这里是天津师范大学，创新创业工作室[官网](https://c422.github.io)的工程仓库页面。本项目使用 [hugo-academic](https://github.com/gcushen/hugo-academic) 作为项目模版，对外观进行了轻微的自定义。此外，本仓库启用了 [Travis-CI 流水线](https://travis-ci.org/c422/c422-src/builds)，，当 master 分支更新后，可以自动部署，无需手动编译。
+这里是天津师范大学，iOS[官网](https://TJNU-iOS-Club.github.io)的工程仓库页面。本项目使用 [hugo-academic](https://github.com/gcushen/hugo-academic) 作为项目模版，对外观进行了轻微的自定义。此外，本仓库启用了 [Travis-CI 流水线](https://travis-ci.org/c422/c422-src/builds)，，当 master 分支更新后，可以自动部署，无需手动编译。
 
 ## 网站介绍
 
@@ -23,8 +23,8 @@
 获取代码
 
 ```bash
-git clone https://github.com/c422/c422-src.git
-cd c422-src
+git clone https://github.com/TJNU-iOS-Club/BlogSrc-Of-TJNU-iOS-Club.git
+cd BlogSrc-Of-TJNU-iOS-Club
 
 # 更新 submodule
 git submodule update --init --recursive
--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,14 @@
 set -e
 
 git config --global credential.helper store
-git config --global user.email "hawkinszhao@outlook.com"
-git config --global user.name "Yiqin Zhao"
+git config --global user.email "kim_yang95@163.com"
+git config --global user.name "KimYangOfCat"
 git config --global push.default simple
 
 rm -rf deployment
-git clone -b master https://yiqinzhao:$GITHUB_TOKEN@github.com/c422/c422.github.io deployment
-rsync -av --delete --exclude ".git" public/ deployment
+git clone -b master https://KimYangOfCat:$GITHUB_TOKEN@github.com/TJNU-iOS-Club/TJNU-iOS-Club.github.io deployment
+rsync -av --delete --exclude ".git" --exclude "CNAME"  public/ deployment
+# 在构建新的页面之前会将之前的界面全部删除（delete），exclude是排除一些不删除的选项！
 cd deployment
 git add -A
 # we need the || true, as sometimes you do not have any content changes
